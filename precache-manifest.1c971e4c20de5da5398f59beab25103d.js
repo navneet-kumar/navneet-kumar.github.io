16a1907afc3bf7d48721110b90c4756294087a41
Navneet kumar <ts-navneet.kumar@rakuten.com>
2020-05-08 13:52:54 +0900

    Updates

---
+++ precache-manifest.1c971e4c20de5da5398f59beab25103d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43001c3168d5002aac42bdcf8cd1d3a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13a8e09dcfec42ba326a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5dfa6fd6deeea48cca70",
+    "url": "/static/js/2.e1c18b12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e1c18b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a8e09dcfec42ba326a",
+    "url": "/static/js/main.380f04f2.chunk.js"
+  },
+  {
+    "revision": "e5cca7410394a25fcee4",
+    "url": "/static/js/runtime-main.e689eb0c.js"
+  }
+]);
--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-import moment from "moment";
-
-export function age(from) {
-  var years = moment().diff(from, "years");
-  var month = moment().diff(from, "months");
-  var days = moment().diff(from, "days");
-  return [years + " year ", month + " months ", days + " days "];
-}
